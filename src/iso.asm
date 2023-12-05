759c6f9dd36a64798aeae241272f090a51aa4167
pmwasson <pmwasson@gmail.com>
2023-12-04 19:20:19 -0800

    initial working build

--- src/iso.asm
+++ src/iso.asm
@@ -42,8 +42,8 @@ mapOffset:  .byte   4+5*8
 ;       combine order: even row (0,-1) lower-right(4,12), odd row (0,0) lower-left (0,8), even-row (0,+1) upper-right(4,4), odd row (0,2) upper-left(0,0)
 ;       then           even row (1,-1) lower-left (0,12), odd row (0,0) lower-right(4,8), even-row (1,+1) upper-left (0,4), odd row (0,2) upper-left(4,0)
 ;    2) odd row tile lower half left (4x4) then right (4x4) across screen
-;       combine order: odd row (0,0) lower-left (0,12), even-row (0,+1) lower-right(4,8), odd row (0,2) upper-left(0,4), even row(0,+3) upper-right(0,0)
-;       then           odd row (0,0) lower-right(4,12), even-row (1,+1) lower-left (0,8), odd row (0,2) upper-left(4,4), even row(1,+3) upper-left(4,0)
+;       combine order: odd row (0,0) lower-left (0,12), even-row (0,+1) lower-right(4,8), odd row (0,2) upper-left(0,4), even row(0,+3) upper-right(4,0)
+;       then           odd row (0,0) lower-right(4,12), even-row (1,+1) lower-left (0,8), odd row (0,2) upper-left(4,4), even row(1,+3) upper-left(0,0)
 ;
 ;
 ;  The left->right can share the odd row and next odd row tiles
@@ -67,11 +67,10 @@ mapOffset:  .byte   4+5*8
     ;
     ; 11: 88 .. 95
 
-    lda     #5*8
+    lda     #1*8
     sta     mapOffset
 :
     jsr     isoDrawMapTile
-
     inc     mapOffset
     lda     mapOffset
     and     #$7         ; are the lower 3 bits zero?
@@ -81,7 +80,7 @@ mapOffset:  .byte   4+5*8
     lda     mapOffset
     adc     #8
     sta     mapOffset
-    cmp     #7*8
+    cmp     #15*8
     bne     :-
 
 
@@ -104,6 +103,9 @@ mapOffset:  .byte   4+5*8
     lsr
     lsr
     lsr
+    lsr
+    clc
+    adc     #8
     sta     tileY
 
     jsr     isoDrawTile_oddRow
@@ -181,11 +183,7 @@ mapOffset:  .byte   4+5*8
     ; draw lower-left 4x4
     ;=====================
 
-    ; screenPtr1 is good, restore 0
-    clc
-    lda     screenPtr0
-    adc     #32             ; 4 rows of 8 bytes
-    sta     screenPtr0
+    ; screen pointer is ready for lower
 
     ; odd row (0,0) lower-left (0,12)
     ; Copy from B -> A
@@ -220,7 +218,7 @@ mapOffset:  .byte   4+5*8
     ora     #$80
     sta     ptrCB0   
 
-    ; even row(0,+3) upper-right(0,0)
+    ; even row(0,+3) upper-right(4,0)
 
     lda     mapOffset
     clc
@@ -231,7 +229,7 @@ mapOffset:  .byte   4+5*8
     adc     currentSheet_56x16+1
     sta     ptrDA1
     sta     ptrDB1 
-    lda     #0+0*8
+    lda     #2+0*8
     sta     ptrDA0
     ora     #$80
     sta     ptrDB0     
@@ -305,11 +303,7 @@ mapOffset:  .byte   4+5*8
     ; draw lower-right 4x4
     ;=====================
 
-    ; screenPtr1 is good, restore 0
-    clc
-    lda     screenPtr0
-    adc     #32             ; 4 rows of 8 bytes
-    sta     screenPtr0
+    ; screen pointer is ready
 
     ; odd row (0,0) lower-right(4,12)
     ; Copy from B -> A
@@ -344,7 +338,7 @@ mapOffset:  .byte   4+5*8
     ora     #$80
     sta     ptrCB0   
 
-    ; even row(1,+3) upper-left(4,0)
+    ; even row(1,+3) upper-left(0,0)
     lda     mapOffset
     clc
     adc     #MAP_S2E
@@ -354,7 +348,7 @@ mapOffset:  .byte   4+5*8
     adc     currentSheet_56x16+1
     sta     ptrDA1
     sta     ptrDB1 
-    lda     #2+0*8
+    lda     #0+0*8
     sta     ptrDA0
     ora     #$80
     sta     ptrDB0     
@@ -380,13 +374,11 @@ mapOffset:  .byte   4+5*8
 .proc isoDrawTile4x4
     ; Need 8 pointers (4 pixels + 4 masks)
 
-    ldy     #0
+    lda     #0
+    sta     offset
 
 loop:
-    ; Line data is stored 4 aux, 4 main
-    ; So we need +0,+4,+1,+5
-
-    sta     RAMWRTON            ; aux mem write
+    ldy     offset
 
     ; +0 in aux
     lda     (ptrAA0),y
@@ -396,11 +388,10 @@ loop:
     ora     (ptrCA0),y          ; data
     and     (ptrDB0),y          ; mask
     ora     (ptrDA0),y          ; data
-    sta     (screenPtr0),y      ; y=+0 s=+0 -> 0
+    sta     byte0
 
     iny                         ; y = +1
 
-    ; +1 in aux
     lda     (ptrAA0),y
     and     (ptrBB0),y          ; mask 
     ora     (ptrBA0),y          ; data
@@ -408,57 +399,69 @@ loop:
     ora     (ptrCA0),y          ; data
     and     (ptrDB0),y          ; mask
     ora     (ptrDA0),y          ; data
-    sta     (screenPtr0),y      ; y=+1 s=+0 -> 1
+    sta     byte1
 
     iny                         
     iny     
     iny                         ; y = +4
 
-    sec
-    lda     screenPtr0
-    sbc     #4                  ; s=-4
-    sta     screenPtr0
+    lda     (ptrAA0),y
+    and     (ptrBB0),y          ; mask 
+    ora     (ptrBA0),y          ; data
+    and     (ptrCB0),y          ; mask
+    ora     (ptrCA0),y          ; data
+    and     (ptrDB0),y          ; mask
+    ora     (ptrDA0),y          ; data
+    sta     byte2
+
+    iny                         ; y = +5
+
+    lda     (ptrAA0),y
+    and     (ptrBB0),y          ; mask 
+    ora     (ptrBA0),y          ; data
+    and     (ptrCB0),y          ; mask
+    ora     (ptrCA0),y          ; data
+    and     (ptrDB0),y          ; mask
+    ora     (ptrDA0),y          ; data
+    sta     byte3
+
+    iny                         
+    iny     
+    iny                         ; y = +8
+    sty     offset
+
+    sta     RAMWRTON            ; aux mem write
+    ldy     #0
+    lda     byte0
+    sta     (screenPtr0),y      ; aux+0
+    ldy     #1
+    lda     byte1
+    sta     (screenPtr0),y      ; aux+1
 
     sta     RAMWRTOFF           ; main mem write
-
-    lda     (ptrAA0),y
-    and     (ptrBB0),y          ; mask 
-    ora     (ptrBA0),y          ; data
-    and     (ptrCB0),y          ; mask
-    ora     (ptrCA0),y          ; data
-    and     (ptrDB0),y          ; mask
-    ora     (ptrDA0),y          ; data
-    sta     (screenPtr0),y      ; y=+4 s=-4 -> 0
-
-    iny
-
-    lda     (ptrAA0),y
-    and     (ptrBB0),y          ; mask 
-    ora     (ptrBA0),y          ; data
-    and     (ptrCB0),y          ; mask
-    ora     (ptrCA0),y          ; data
-    and     (ptrDB0),y          ; mask
-    ora     (ptrDA0),y          ; data
-    sta     (screenPtr0),y      ; y=+5 s=-4 -> 1
-
-    sec
-    lda     screenPtr0
-    sbc     #4                  ; s=-4
-    sta     screenPtr0
-
-    clc
-    tya
-    adc     #8-5               
-    tay                         ; y=+8 s=-8 -> 0
+    ldy     #0
+    lda     byte2
+    sta     (screenPtr0),y      ; main+0
+    ldy     #1
+    lda     byte3
+    sta     (screenPtr0),y      ; main+1
 
     lda     screenPtr1
     adc     #4
     sta     screenPtr1
 
-    cpy     #8*4
-    bne     loop
-
+    lda     offset
+    cmp     #8*4
+    beq     :+
+    jmp     loop
+:
     rts
+
+offset:     .byte   0
+byte0:      .byte   0
+byte1:      .byte   0
+byte2:      .byte   0
+byte3:      .byte   0
 
 .endproc
 
@@ -469,35 +472,40 @@ loop:
 .align 256
 isoMap:
 
-    .byte   00, 00, 00, 00, 00, 00, 00, 00      ; row  0
-    .byte     00, 00, 00, 02, 00, 00, 00, 00    ;      1
-    .byte   00, 00, 00, 01, 01, 00, 00, 00      ;      2
-    .byte     00, 00, 01, 01, 01, 00, 00, 00    ;      3
-    .byte   00, 00, 01, 02, 01, 01, 00, 00      ;      4
-    .byte     00, 01, 01, 01, 01, 01, 00, 00    ;      5
-    .byte   00, 00, 01, 01, 01, 04, 01, 00      ;      6
-    .byte     00, 00, 02, 01, 01, 01, 00, 00    ;      7
-    .byte   00, 00, 00, 01, 01, 01, 00, 00      ;      8
-    .byte     00, 00, 00, 01, 02, 00, 00, 00    ;      9
-    .byte   00, 00, 00, 00, 01, 00, 00, 00      ;     10
-    .byte     00, 00, 00, 00, 00, 00, 00, 00    ;     11
+    .byte    0,  0,  0,  0,  0,  0,  0,  0      ; row  0
+    .byte      0,  0,  0, 01, 01,  0,  0,  0    ;      1
+    .byte    0,  0,  0, 01, 01, 00,  0,  0      ;      2
+    .byte      0,  0, 01, 01, 00,  1,  0,  0    ;      3
+    .byte    0,  0, 01, 01, 00, 12,  0,  0      ;      4
+    .byte      0, 01, 01, 00, 12, 09,  0,  0    ;      5
+    .byte    0,  0, 01, 11, 12, 11,  0, 0       ;      6
+    .byte      0, 07, 00,  1, 01, 01,  0,  0    ;      7
+    .byte    0,  0, 08,  1, 01, 01, 01,  0      ;      8
+    .byte      0,  0, 00, 01, 01, 01,  0,  0    ;      9
+    .byte    0,  0,  0, 07, 01, 01, 09,  0      ;     10
+    .byte      0,  0,  0, 07, 01, 09,  0,  0    ;     11
+    .byte    0,  0,  0,  0, 07, 09,  0,  0      ;     12
+    .byte      0,  0,  0,  0, 08,  0,  0,  0    ;     13
+    .byte    0,  0,  0,  0,  0,  0,  0,  0      ;     14
+    .byte      0,  0,  0,  0,  0,  0,  0,  0    ;     15
 
 
-    .byte  01, 01, 01, 01, 01, 01, 01, 01
-    .byte    01, 01, 01, 01, 01, 01, 01, 01
-    .byte  01, 01, 01, 01, 01, 01, 01, 01
-    .byte    01, 01, 01, 01, 01, 01, 01, 01
-    .byte  01, 01, 01, 01, 01, 01, 01, 01
-    .byte    01, 01, 01, 01, 01, 01, 01, 01
-    .byte  01, 01, 01, 01, 01, 01, 01, 01
-    .byte    01, 01, 01, 01, 01, 01, 01, 01
-    .byte  01, 01, 01, 01, 01, 01, 01, 01
-    .byte    01, 01, 01, 01, 01, 01, 01, 01
-    .byte  01, 01, 01, 01, 01, 01, 01, 01
-    .byte    01, 01, 01, 01, 01, 01, 01, 01
-
-
-
+    .byte  01, 01, 01, 01, 01, 01, 01, 01     ; row  0  
+    .byte    01, 01, 01, 01, 01, 01, 01, 01   ;      1 <--      
+    .byte  01, 01, 01, 01, 01, 01, 01, 01     ;      2  
+    .byte    01, 01, 01, 01, 01, 01, 01, 01   ;      3      
+    .byte  01, 01, 01, 01, 01, 01, 01, 01     ;      4  
+    .byte    01, 01, 01, 01, 01, 01, 01, 01   ;      5      
+    .byte  01, 01, 01, 01, 01, 01, 01, 01     ;      6  
+    .byte    01, 01, 01, 01, 01, 01, 01, 01   ;      7      
+    .byte  01, 01, 01, 01, 01, 01, 01, 01     ;      8  
+    .byte    01, 01, 01, 01, 01, 01, 01, 01   ;      9      
+    .byte  01, 01, 01, 01, 01, 01, 01, 01     ;     10  
+    .byte    01, 01, 01, 01, 01, 01, 01, 01   ;     11 <--      
+    .byte  01, 01, 01, 01, 01, 01, 01, 01     ;     12
+    .byte    01, 01, 01, 01, 01, 01, 01, 01   ;     13
+    .byte  01, 01, 01, 01, 01, 01, 01, 01     ;     14
+    .byte    01, 01, 01, 01, 01, 01, 01, 01   ;     15
 
 
 
